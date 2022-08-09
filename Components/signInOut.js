c5fd6d8de91b4db69dcd0772173a3a53bcc6b122
rachelalk <100844754+rachelalk@users.noreply.github.com>
2022-08-09 10:01:58 +0100

    installed next-images package and updated logo on pre-login page

--- Components/signInOut.js
+++ Components/signInOut.js
@@ -4,18 +4,18 @@ import LinkButton from './linkButton'
 
 const SignInOut = () => {
     return (
-        <div className='not-fund'>
-         <div>
-          <Image
-              className="home-image"
-              src={require("./../public/conCAT.png")}
-              alt="Picture of cat and dog"
-              />
-         </div>
-        <h2>Welcome to The Care-Full App</h2>
-        <LinkButton text="Log In" link="/api/auth/login"/>
-        </div>
-    )
+			<div className="not-fund">
+				<div>
+					<Image
+						className="home-image"
+						src={require("./../public/clear_logo.png")}
+						alt="Care-Full App Logo"
+					/>
+				</div>
+				<h2>Welcome to The Care-Full App</h2>
+				<LinkButton text="Log In" link="/api/auth/login" />
+			</div>
+		);
 }
 
 export default SignInOut
--- next.config.js
+++ next.config.js
@@ -3,5 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
+const withImages = require("next-images");
+module.exports = withImages({
+	esModule: true,
+	fileExtensions: ["jpg", "jpeg", "png", "gif"],
+	webpack(config, options) {
+		return config;
+	},
+});
 
 module.exports = nextConfig
